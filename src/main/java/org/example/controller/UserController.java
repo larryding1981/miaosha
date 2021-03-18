22ef77d281a879fe9d12f4b76f7096243087a744
dingjianjian <dingjianjian1981@126.com>
2021-03-18 14:12:37 +0800

    添加没添加完的文件

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,75 @@
+package org.example.controller;
+
+import org.example.controller.viewobject.UserVO;
+import org.example.error.BusinessException;
+import org.example.error.EmBusinessError;
+import org.example.response.CommonReturnType;
+import org.example.service.UserService;
+import org.example.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@Controller("user")
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController extends BaseController{
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取opt短信接口
+    @RequestMapping(value = "/getotp", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone){
+        //需要按一定规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码同对应用户的手机号相关联，使用httpsession的方式绑定他的手机号与otpCode
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        //将otp验证码通过短信通道发送给用户
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) throws BusinessException {
+        //调用service服务获取对应id的用户对象并返回给前端
+        UserModel userModel = userService.getUserById(id);
+
+        if(userModel ==  null){
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
+        //将核心领域模型用户对象转化为可供UI使用的viewobject
+        UserVO userVO = convertFromModel(userModel);
+
+        return CommonReturnType.create(userVO);
+    }
+
+    private UserVO convertFromModel(UserModel userModel){
+        if(userModel == null){
+            return null;
+        }
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel, userVO);
+        return userVO;
+    }
+
+}
---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+import org.example.service.model.UserModel;
+
+public interface UserService {
+    UserModel getUserById(Integer id);
+}
